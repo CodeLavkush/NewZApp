e334f75a1a31058093e6e8671359fd3368b96950
codelavkush <mauryalavkush19@gmail.com>
2024-09-27 07:40:43 +0530

    navbar close button updated

--- public/scripts/navbar.js
+++ public/scripts/navbar.js
@@ -3,12 +3,14 @@ let closeBtn = document.querySelector(".close-btn");
 
 menuBtn.addEventListener('click', ()=>{
     let sideNavbar = document.querySelector(".navbar-side");
-
+    menuBtn.style.display = 'none';
+    closeBtn.style.display = 'flex'
     sideNavbar.style.display = 'flex';
 })
 
 closeBtn.addEventListener('click', ()=>{
     let sideNavbar = document.querySelector(".navbar-side");
-
+    menuBtn.style.display = 'flex';
+    closeBtn.style.display = 'none';
     sideNavbar.style.display = 'none';
 })
